2d35eb8262fd3f597680e14afa55c5a6c8eb01d4
bouzuya <m@bouzuya.net>
2021-06-20 16:46:08 +0900

    remove entry_id from examples/get_entry.rs

--- examples/get_entry.rs
+++ examples/get_entry.rs
@@ -1,10 +1,13 @@
+use std::env;
+
 use hatena_blog::{Client, Config};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let id = env::args().nth(1).unwrap();
     let config = Config::new_from_env()?;
     let client = Client::new(&config);
-    let response_body = client.get_entry("26006613776339413").await?;
+    let response_body = client.get_entry(id.as_str()).await?;
     println!("{:?}", response_body);
     Ok(())
 }
